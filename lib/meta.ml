d888074b5608c05027274ed121e04a0371e0bd43
Dmitry Ponyatov <dponyatov@samsa.local>
2024-03-28 12:53:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@samsa.local>

--- lib/meta.ml
+++ lib/meta.ml
@@ -2,9 +2,19 @@
 
 let meta = "meta" ;;
 let perm = 0o755 ;;
-let dirs = [ "bin"; "doc"; "lib"; "inc"; "src"; "tmp" ] ;;
+let dirs = [ ".vscode" ; "bin"; "doc"; "lib"; "inc"; "src"; "tmp" ] ;;
 let dirs = [ meta ] @ List.map (fun s -> meta ^ "/" ^ s) dirs ;;
-let mk dir = try Sys.mkdir dir perm with Sys_error _ -> () ;;
+let mk dir = if not (Sys.file_exists dir ) then Sys.mkdir dir perm ;;
 
 List.map mk dirs ;;
+
+let giti = List.map open_out (List.map (fun dir -> dir ^ "/.gitignore") dirs);;
+List.map (fun ch -> Printf.fprintf ch "!.gitignore\n") giti;;
+List.map (fun ch -> flush ch) giti;;
+
+let mk = open_out (meta ^ "/Makefile");;
+
+let apt = open_out (meta ^ "/apt.txt");;
+
+let readme = open_out (meta ^ "/README.md");;
 
