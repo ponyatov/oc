631a110d7d78b791763121eec3a5c06452a7e764
Dmitry Ponyatov <dponyatov@samsa.local>
2024-03-28 12:04:43 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@samsa.local>

--- lib/meta.ml
+++ lib/meta.ml
@@ -1,6 +1,10 @@
 (* metaCompiler *)
 
-let meta = "meta"
-let perm = 0o755;;
+let meta = "meta" ;;
+let perm = 0o755 ;;
+let dirs = [ "bin"; "doc"; "lib"; "inc"; "src"; "tmp" ] ;;
+let dirs = [ meta ] @ List.map (fun s -> meta ^ "/" ^ s) dirs ;;
+let mk dir = try Sys.mkdir dir perm with Sys_error _ -> () ;;
 
-try Sys.mkdir meta perm with Sys_error _ -> ()
+List.map mk dirs ;;
+
